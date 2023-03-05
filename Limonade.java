7895c5801f5bd34de5bf591f424243e79312efc0
Irina <airin-lia@mail.ru>
2023-03-05 23:16:24 +0300

    Derived class 2

---
+++ Limonade.java
@@ -0,0 +1,12 @@
+package OOP_HW1;
+
+public class Limonade extends Drink{
+    
+    public Limonade(String name, Double price, Integer quantity, String measure, Double volume){
+        super(name, price, quantity, measure, volume);
+    }
+@Override
+public String toString() {
+    return super.toString();
+}
+    }
