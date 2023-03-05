05e54919d0fa07bfc24e7a37d955f2e11003bc17
Irina <airin-lia@mail.ru>
2023-03-05 23:16:59 +0300

    Derived class 2

---
+++ Pacifier.java
@@ -0,0 +1,12 @@
+package OOP_HW1;
+
+public class Pacifier extends Children{
+    
+    public Pacifier(String name, Double price, Integer quantity, String measure, Integer minAge, String hypoallergic){
+    super(name, price, quantity, measure, minAge, hypoallergic);
+    }
+@Override
+public String toString() {
+    return super.toString();
+}
+}
