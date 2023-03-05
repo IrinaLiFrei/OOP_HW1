469b27ee51345a4cc7864276388402517c00284f
Irina <airin-lia@mail.ru>
2023-03-05 23:16:38 +0300

    Derived class 2

---
+++ Mask.java
@@ -0,0 +1,11 @@
+package OOP_HW1;
+
+public class Mask extends Hygiene {
+    public Mask(String name, Double price, Integer quantity, String measure, Integer numOfPieces) {
+        super(name, price, quantity, measure, numOfPieces);
+    }
+    @Override
+    public String toString() {
+       return super.toString();
+    }
+}
